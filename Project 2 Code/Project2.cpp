3215f60c701bcd5444e0549e2512037f18321969
mgunnett <m-gunnett@onu.edu>
2025-11-04 20:08:35 -0500

    Added cout statements to main()

--- Project 2 Code/Project2.cpp
+++ Project 2 Code/Project2.cpp
@@ -5,5 +5,27 @@
 using namespace std;
 
 int main() {
-    
+    // TO BE REMOVED AND REPLACED ONCE DONE
+    int replaceMe = 0;
+
+    cout << "Linear Search:\n";
+    cout << "----------------\n";
+    cout << "elapsed time for run #1 - ", replaceMe, "\n";
+    cout << "elapsed time for run #2 - ", replaceMe, "\n";
+    cout << "elapsed time for run #3 - ", replaceMe, "\n";
+    cout << "elapsed time for run #4 - ", replaceMe, "\n";
+    cout << "elapsed time for run #5 - ", replaceMe, "\n\n";
+    cout << "average search time: ", replaceMe, "\n";
+    cout << "number of comparisons made: ", replaceMe, "\n";
+    cout << "\n\n\n\n";
+    cout << "Binary Search:\n";
+    cout << "-----------------\n";
+    cout << "elapsed time for run #1 - ", replaceMe, "\n";
+    cout << "elapsed time for run #2- ", replaceMe, "\n";
+    cout << "elapsed time for run #3 - ", replaceMe, "\n";
+    cout << "elapsed time for run #4 - ", replaceMe, "\n";
+    cout << "elapsed time for run #5 - ", replaceMe, "\n\n";
+    cout << "average search time: ", replaceMe, "\n";
+    cout << "sorting time: ", replaceMe, "\n";
+    cout << "number of comparisons made: ", replaceMe, "\n";
 }
